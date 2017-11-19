5fb4ce2ae963ec81e90dd9be9e60990d930658df
Bogdan Timchenko <bogdantimchenko@yandex.ru>
2017-11-19 20:05:44 +0300

    Timchenko Bogdan, P3496, HW3

--- src/Tasks/GADT_1/GADTParser.hs
+++ src/Tasks/GADT_1/GADTParser.hs
@@ -13,11 +13,12 @@ import           Text.Parsec.Prim       (many, parseTest, try, (<|>))
 import           Text.Parsec.Text       (Parser)
 import           Text.Parsec.Token
 
+
 iLitP :: Parser (Lit Int)
-iLitP = undefined
+iLitP = ILit <$> (read <$> many1 digit) 
 
 bLitP :: Parser (Lit Bool)
-bLitP = undefined
+bLitP = try $ (\x -> BLit (x == 'T')) <$> ((char 'T') <|> (char 'F'))
 
 iiLitP :: Parser (Expr Int)
 iiLitP = Lit <$> iLitP
@@ -26,24 +27,29 @@ bbLitP :: Parser (Expr Bool)
 bbLitP = Lit <$> bLitP
 
 addP :: Parser (Expr Int)
-addP = undefined
+addP = Add <$> (((try $ spacedP iiLitP) <|> (try $ bracketP parse)) <* many space <* char '+') <* many space <*> parse
 
 leqP :: Parser (Expr Bool)
 leqP = Leq <$> (parse <* char '<') <*> parse
 
 andP :: Parser (Expr Bool)
-andP = undefined
+andP = And <$> (((try $ spacedP leqP) <|> 
+                 (try $ spacedP bbLitP) <|> 
+                 (try $ bracketP parse) ) <* 
+                 many space <* string "&&") <* 
+       many space <*> parse
 
 spacedP :: Parser a -> Parser a
-spacedP p = (many space *> p) <* many space
+spacedP p = many space *> p <* many space
 
 bracketP :: Parser a -> Parser a
-bracketP = undefined
+bracketP p = try $ between (char '(') (char ')') $ p
 
 class MyParse a where
   parse :: Parser (Expr a)
 
 instance MyParse Int where
-  parse = undefined
+  parse = (try $ spacedP addP) <|> (try $ spacedP iiLitP) <|>(try $ bracketP $ spacedP parse)
+
 instance MyParse Bool where
-  parse = undefined
+  parse = (try $ spacedP andP) <|> (try $ spacedP leqP) <|> (try $ spacedP bbLitP) <|> (try $ bracketP $ spacedP parse) 
